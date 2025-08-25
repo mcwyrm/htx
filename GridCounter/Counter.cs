0171a0aeeb933c4b03b34e3fea08598c1db19c4c
Bruce Zink <bruce@zinkdesign.net>
2025-08-24 23:05:40 -0400

    initial implementation

---
+++ GridCounter/Counter.cs
@@ -0,0 +1,25 @@
+﻿using System.Diagnostics;
+
+namespace GridCounter
+{
+    public class Counter
+    {
+        public int Count(int gridSize)
+        {
+            Debug.Assert(gridSize > 0);
+            int count = 0;
+            for (int countSize = 1; countSize <= gridSize; countSize++)
+            {
+                for (int vertical = 0; vertical <= gridSize - countSize; vertical++)
+                {
+                    for (int horizontal = 0; horizontal <= gridSize - countSize; horizontal++)
+                    {
+                        count++;
+                    }
+                }
+            }
+            return count;
+        }
+    }
+}
+
---
+++ GridCounterTests/GridCounterTests.cs
@@ -0,0 +1,17 @@
+﻿namespace GridCounterTests
+{
+    [TestClass]
+    public sealed class GridCounterTests
+    {
+        [TestMethod]
+        [DataRow(1, 1)]
+        [DataRow(2, 5)]
+        [DataRow(3, 14)]
+        public void TestCount(int gridSize, int expectedCount)
+        {
+            var actualCount = new GridCounter.Counter().Count(gridSize);
+
+            Assert.AreEqual(expectedCount, actualCount);
+        }
+    }
+}
